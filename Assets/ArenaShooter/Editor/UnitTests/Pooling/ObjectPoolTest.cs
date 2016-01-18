d9be4355f79df479888c520d5f37280d9147dc85
John Winder <jwinder86@gmail.com>
2016-01-18 14:32:24 -0800

    Removing Unity Test Tools
    UTT is only compatible with version 4.x, while version 5.x has built-in unit testing tools.  UTT is not needed.

---
+++ Assets/ArenaShooter/Editor/UnitTests/Pooling/ObjectPoolTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+[TestFixture]
+[Category("Pooling")]
+public class ObjectPoolTest {
+
+    [Test]
+    public void sampleTest() {
+        Assert.Pass();
+    }
+}
