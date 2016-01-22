8c6541548450dd93ac62536f7fa3ce85e768d8f7
bertmsanders <mike@sanders-tech.com>
2016-01-21 22:03:05 -0600

    added rudimentary enemy and enemy follow script

--- Assets/ArenaShooter/Scripts/Runtime/Enemies/EnemyBehaviour.cs
+++ Assets/ArenaShooter/Scripts/Runtime/Enemies/EnemyBehaviour.cs
@@ -5,7 +5,7 @@ using System.Collections;
 [RequireComponent(typeof(Collider))]
 public class EnemyBehaviour : MonoBehaviour {
 
-    public float maxSpeed;
+    //public float maxSpeed;
     public float accelleration;
 
     public float playerFollowDistance;
@@ -25,29 +25,38 @@ public class EnemyBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("enemy position: " + transform.position + " - Player Position: " + playerLocation.position);
+        //Debug.Log("enemy position: " + transform.position + " - Player Position: " + playerLocation.position +
+        //    " - magnitude: " + (transform.position - playerLocation.position).magnitude + " - distance " + playerFollowDistance);
         if ((transform.position - playerLocation.position).magnitude < playerFollowDistance)
         {
-            FollowPlayer();
+           // Debug.Log("follow player");
+            FollowPlayer(accelleration);
         }
     }
 
 
-    void FollowPlayer()
+    void FollowPlayer(float accelleration)
     {
-        Vector3 direction = (playerLocation.position - transform.position).normalized;
+       
+        Vector3 variance = Random.onUnitSphere * 0.8F;
         
-        if (Vector3.Angle(direction, transform.right) > playerFollowAngle)
-        {
-            return;
-        }
+        Vector3 direction = (playerLocation.position - transform.position + variance).normalized;
 
-        float angle = Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x);
+       /* Debug.Log("playerLocation: " + playerLocation.position);
+        Debug.Log("enemyLocation: " + transform.position);
+        Debug.Log("variance: " + variance);
+        Debug.Log("Direction: " + direction);*/
+
+        /* if (Vector3.Angle(direction, transform.right) > playerFollowAngle)
+         {
+             return;
+         }*/
+
+        //float angle = Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x);
         //transform.position + direction, Quaternion.Euler(new Vector3(0f, 0f, angle)));
+        //rb.AddForce(new Vector3(0f, 0f, angle), ForceMode.Acceleration);
+        // rb.AddRelativeForce(direction, ForceMode.Acceleration);
 
-
-
-
-        rb.AddForce(new Vector3(0f, 0f, angle), ForceMode.Acceleration);
+       rb.AddForce(direction * accelleration, ForceMode.Acceleration);
     }
 }
